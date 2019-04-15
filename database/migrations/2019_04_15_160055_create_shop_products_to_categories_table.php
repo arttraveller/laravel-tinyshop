641ae2362c455fbdb77cd089f473e72143015ef8
artorX <alx>
2019-04-15 19:36:10 +0300

    Added additional product categories

---
+++ database/migrations/2019_04_15_160055_create_shop_products_to_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopProductsToCategoriesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_products_to_categories', function (Blueprint $table) {
+            $table->bigInteger('product_id')->index();
+            $table->bigInteger('category_id')->index();
+
+            $table->primary(['product_id', 'category_id']);
+
+            // FK
+            $table->foreign('product_id')->references('id')->on('shop_products')->onDelete('CASCADE');
+            $table->foreign('category_id')->references('id')->on('shop_categories')->onDelete('CASCADE');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_products_to_categories');
+    }
+
+}
